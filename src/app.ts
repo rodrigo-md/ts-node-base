80bf0b6e33fe4e07362a81304a67eb8a5577f15e
Rodrigo Martinez Diaz <martinez.d.rodrigo@gmail.com>
2021-03-13 01:43:33 -0300

    chore(eslintrc): unistall eslint-plugin-prettier and fix config
    upgrade eslint to the last configuration, specifiy eslint-plugin-prettier and config-prettier aren't
    needed anymore

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,5 @@
 import { sum } from './utils/example';
 import { PORT } from './config/constants';
 
-console.log('sum(3,5) equals ', sum(3, 5));
+console.log('sum(3,5) equals', sum(3, 5));
 console.log('PORT=', PORT);
